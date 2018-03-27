4228d0ecd7b33110ede7d1387109f510e5fa497b
Joannes Paanakker <j.paanakker@upcmail.nl>
2018-03-27 11:21:48 +0200

    add preventions and fixes

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :challenges do
     resources :benefits, only: [:new, :create, :show, :index]
-    resources :fears, only: [:new, :create, :show, :destroy] do 
-      resources :preventions, only: [:new, :create, :show, :destroy]
-      resources :fixes, only: [:new, :create, :show, :destroy]
-    end
+    resources :fears, only: [:new, :create, :show, :destroy]
   end
-
+  resources :fears, only: [:show] do
+    resources :preventions, only: [:new, :create, :show, :destroy]
+    resources :fixes, only: [:new, :create, :show, :destroy]  
+  end
 end
