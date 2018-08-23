2f59aa812b8f37bced7d9502393d0c12e36958be
Joannes Paanakker <j.paanakker@upcmail.nl>
2018-08-23 17:25:14 +0200

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  acts_as_token_authenticatable
+  # acts_as_token_authenticatable
   has_many :challenges, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  
+
 end
